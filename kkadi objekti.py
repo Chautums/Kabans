f47f4396ca0c763aba32821716c80556f70858e5
Chautums <chautums@gmail.com>
2022-04-14 14:13:06 +0300

    master

--- kkadi objekti.py
+++ kkadi objekti.py
@@ -31,3 +31,23 @@
 # p.set("Peter", 20)
 # print(p._person__name) #var piekļūt slēptajam laukam
 # p.output()
+
+
+
+# class person:
+#     def __init__(self,name,age): #konstruktora definēšana
+#         self.name=name #izveido lauku name
+#         self.age=age #izveido lauku age
+#     def output(self): #metodes definēšana
+#         print(self.name,self.age)
+#     def __del__(self):  #destruktora definēšana 
+#         print("Deleted:", self.name, self.age)
+# p=person("Peter",20) #objekta izveidošana un konstruktora izsaukšana
+# p.output()
+# del p #objekta likvidēšana un destruktora izsaukšana
+
+
+class person:
+    counter=0 #klases līmeņa lauks
+    def __init__(self,name,age):
+        
