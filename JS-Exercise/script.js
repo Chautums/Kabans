1dc5d3a992066cb78e5c16c15230136bb2113e74
Chautums <chautums@gmail.com>
2022-11-08 15:28:15 +0200

    master

---
+++ JS-Exercise/script.js
@@ -0,0 +1,21 @@
+const ourFirstDatoriumObject = {
+    classNo: 12,
+    numberOfStudents: 150,
+    skullEmoji:"💀",
+    sunEmoji:"🌞",
+    level: 'Advanced',
+    mood: 'good',
+    money: null
+}
+
+const allDatoriumObjects = {
+    firstProject : ourFirstDatoriumObject,
+    secondProject: {
+        name: "TypeScript project",
+        level: {
+            hard: "very"
+        }
+    },
+    thirdsProject: {name: "JAVA project"},
+    fourthProject: {name: "C# project"},
+}
