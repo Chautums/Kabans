dcdf3e492477344240dbbcc9807c778290d1e123
Chautums <chautums@gmail.com>
2022-12-12 15:16:16 +0200

    master

--- JS-Exercise/barbariska.js
+++ JS-Exercise/barbariska.js
@@ -4,26 +4,12 @@
 // const headingElementObject = document.querySelector('.js-heading')
 // headingElementObject.classList.add('active')
 
-const boxElementObject = document.querySelector(".js-box")
-console.log("boxElementObject", boxElementObject(0));
-boxElementObjects[0].style.width = "200px"
-boxElementObjects[0].style.height = "200px"
-boxElementObjects[0].style.margin = "10px"
-boxElementObjects[0].style.backgroundColor = "red"
+// const boxElementObject = document.querySelector(".js-box")
+// console.log("boxElementObject", boxElementObject(0));
 
-boxElementObjects[1].style.width = "200px"
-boxElementObjects[1].style.height = "200px"
-boxElementObjects[1].style.margin = "10px"
-boxElementObjects[1].style.backgroundColor = "red"
-
-boxElementObjects[2].style.width = "200px"
-boxElementObjects[2].style.height = "200px"
-boxElementObjects[2].style.margin = "10px"
-boxElementObjects[2].style.backgroundColor = "red"
-
-boxElementObjects.forEach((element) => {
-    element.style.width = "200px",
-    element.style.height = "200px",
-    element.style.margin = "10px",
-    element.style.backgroundColor = "red";
-  })
+// boxElementObjects.forEach((element) => {
+//     element.style.width = "200px",
+//     element.style.height = "200px",
+//     element.style.margin = "10px",
+//     element.style.backgroundColor = "red";
+//   })
